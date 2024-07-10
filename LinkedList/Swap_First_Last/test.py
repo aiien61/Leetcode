1d36bf4047272bb3de5989e0c639864631cfcc86
aiien61 <aiien61will@gmail.com>
2024-07-10 22:58:11 +0800

    add some type hint

--- LinkedList/Swap_First_Last/test.py
+++ LinkedList/Swap_First_Last/test.py
@@ -1,10 +1,12 @@
 import unittest
+from base import Node, DoublyLinkedList
 from exercise1 import SolutionList
 from typing import List
 
-def linkedlist_to_list(head) -> List[int]:
-    result = []
-    current = head
+
+def linkedlist_to_list(head: Node) -> List[int]:
+    result: List[int] = []
+    current: Node | None = head
     while current:
         result.append(current.value)
         current = current.next
@@ -12,45 +14,45 @@ def linkedlist_to_list(head) -> List[int]:
 
 class TestSwapFirstLast(unittest.TestCase):
     def test_swap_first_last_when_multiple_nodes(self):
-        ll = SolutionList(1)
+        ll: DoublyLinkedList = SolutionList(1)
         ll.append(2)
         ll.append(3)
         ll.append(4)
 
         ll.swap_first_last()
 
-        expected = [4, 2, 3, 1]
-        actual = linkedlist_to_list(ll.head)
+        expected: List[int] = [4, 2, 3, 1]
+        actual: List[int] = linkedlist_to_list(ll.head)
         self.assertEqual(expected, actual)
 
     def test_swap_first_last_when_single_node(self):
-        ll = SolutionList(1)
+        ll: DoublyLinkedList = SolutionList(1)
 
         ll.swap_first_last()
 
-        expected = [1]
-        actual = linkedlist_to_list(ll.head)
+        expected: List[int] = [1]
+        actual: List[int] = linkedlist_to_list(ll.head)
         self.assertEqual(expected, actual)
 
     def test_swap_first_last_when_list_is_empty(self):
-        ll = SolutionList(None)
+        ll: DoublyLinkedList = SolutionList(None)
         ll.head = None
         ll.length = 0
 
         ll.swap_first_last()
 
-        expected = []
-        actual = linkedlist_to_list(ll.head)
+        expected: list = []
+        actual: list = linkedlist_to_list(ll.head)
         self.assertEqual(expected, actual)
 
     def test_swap_first_last_when_two_node(self):
-        ll = SolutionList(1)
+        ll: DoublyLinkedList = SolutionList(1)
         ll.append(2)
 
         ll.swap_first_last()
 
-        expected = [2, 1]
-        actual = linkedlist_to_list(ll.head)
+        expected: List[int] = [2, 1]
+        actual: List[int] = linkedlist_to_list(ll.head)
         self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
